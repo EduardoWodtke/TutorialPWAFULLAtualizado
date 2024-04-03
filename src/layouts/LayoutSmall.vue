f453a7d436a4db7c66f0b80d386b3391b87f112b
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-04-03 16:49:09 -0300

    PWA tutorial full atualizado Seculo XXI ano 2024/04/03

---
+++ src/layouts/LayoutSmall.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import SmallHeader from '../components/templates/small/SmallFooter.vue'
+    import SmallFooter from '../components/templates/small/SmallHeader.vue'
+</script>
+<template>
+    <header>
+        <SmallHeader/>
+    </header>
+    <main>
+        <RouterView/>
+    </main>
+    <footer>
+        <SmallFooter/>
+    </footer>
+</template>
